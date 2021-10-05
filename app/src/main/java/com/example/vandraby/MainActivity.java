8d7dab32164f608531aadb8193ab66cd2a5a2ba0
Vanya Sachkovski <ivan.sachkovski@gmail.com>
2021-10-05 04:18:51 +0300

    update new main activity

--- app/src/main/java/com/example/vandraby/MainActivity.java
+++ app/src/main/java/com/example/vandraby/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.vandraby;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavigationBarView bottomNavigationView = findViewById(R.id.bottom_navigation_panel);
+        bottomNavigationView.setSelectedItemId(R.id.item_profile);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.item_swipes:
+                    SwipesFragment fragment1 =  SwipesFragment.newInstance();
+                    loadFragment(fragment1);
+                    return true;
+                case R.id.item_profile:
+                    ProfileFragment fragment =  ProfileFragment.newInstance();
+                    loadFragment(fragment);
+                    return true;
+            }
+            return false;
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content_fragment, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/vandraby/SwipesFragment.java
@@ -0,0 +1,26 @@
+package com.example.vandraby;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SwipesFragment  extends Fragment {
+    public static SwipesFragment newInstance() {
+        return new SwipesFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_swipes, null);
+    }
+}
