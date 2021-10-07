1aad598628f771d22afafea29bda1a2eae726f39
Vanya Sachkovski <ivan.sachkovski@gmail.com>
2021-10-07 12:42:05 +0300

    Change UI

--- app/src/main/java/com/example/vandraby/activities/main/profile/ProfileFragment.java
+++ app/src/main/java/com/example/vandraby/activities/main/profile/ProfileFragment.java
@@ -1,13 +1,17 @@
 package com.example.vandraby.activities.main.profile;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.vandraby.R;
+import com.example.vandraby.information.DatabaseHandler;
+import com.example.vandraby.information.User;
 
 public class ProfileFragment extends Fragment {
     public static ProfileFragment newInstance() {
@@ -22,7 +26,14 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, null);
 
-        return inflater.inflate(R.layout.fragment_profile, null);
+        DatabaseHandler handler = DatabaseHandler.getInstance(null);
+        User user = handler.getCurrentUser();
+
+        TextView tvFullName = view.findViewById(R.id.user_full_name_view);
+        tvFullName.setText(user.getFullName());
+
+        return view;
     }
 }
