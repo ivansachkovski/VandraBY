8e4c3561e7dc36bb2b3a0fd625f798941448d5b8
Vanya Sachkovski <ivan.sachkovski@gmail.com>
2021-10-15 03:04:31 +0300

    rework list view

--- app/src/main/java/com/example/vandraby/activities/main/profile/ProfileFragment.java
+++ app/src/main/java/com/example/vandraby/activities/main/profile/ProfileFragment.java
@@ -4,21 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.vandraby.R;
+import com.example.vandraby.adapters.ObjectsAdapter;
 import com.example.vandraby.information.DataModel;
-import com.example.vandraby.information.Sight;
 import com.example.vandraby.information.User;
 import com.squareup.picasso.Picasso;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -40,32 +35,17 @@ public class ProfileFragment extends Fragment {
         DataModel dataModel = DataModel.getInstance();
         User user = dataModel.getCurrentUser();
 
-        CircleImageView civUserPhoto = view.findViewById(R.id.user_profile_image);
-        Picasso.with(view.getContext()).load(user.getPhotoUrl()).fit().into(civUserPhoto);
+        CircleImageView imageUserPhoto = view.findViewById(R.id.user_profile_image);
+        Picasso.with(view.getContext()).load(user.getPhotoUrl()).fit().into(imageUserPhoto);
 
-        TextView tvFullName = view.findViewById(R.id.user_full_name_view);
-        tvFullName.setText(user.getFullName());
+        TextView textUserFullName = view.findViewById(R.id.user_full_name_view);
+        textUserFullName.setText(user.getFullName());
 
-        TextView tvNickname = view.findViewById(R.id.user_nickname_view);
-        tvNickname.setText("@" + user.getNickname());
+        TextView textUserNickname = view.findViewById(R.id.user_nickname_view);
+        textUserNickname.setText("@" + user.getNickname());
 
-        ArrayList<Sight> objects = dataModel.getUserLikedObjects();
-        ArrayList<HashMap<String, Object>> data = new ArrayList<>(objects.size());
-
-        for (Sight object: objects) {
-            HashMap<String, Object> item = new HashMap<>();
-            item.put("name", object.getName());
-            item.put("location", object.getLocation());
-            data.add(item);
-        }
-
-        String[] from = {"name", "location"};
-        int[] to = {R.id.tv_name, R.id.tv_location};
-
-        SimpleAdapter likedObjectsAdapter = new SimpleAdapter(getContext(), data, R.layout.list_item, from, to);
-
-        ListView lvLikedObjects = view.findViewById(R.id.lv_liked_objects);
-        lvLikedObjects.setAdapter(likedObjectsAdapter);
+        RecyclerView listLikedObjects = view.findViewById(R.id.lv_liked_objects);
+        listLikedObjects.setAdapter(new ObjectsAdapter(dataModel.getUserLikedObjects()));
 
         return view;
     }
