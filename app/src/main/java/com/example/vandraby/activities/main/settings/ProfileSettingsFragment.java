9ed2295810e3f0cc7db07943a5cd348153b5e7dc
Vanya Sachkovski <ivan.sachkovski@gmail.com>
2021-10-18 03:41:43 +0300

    small fix on settings page

--- app/src/main/java/com/example/vandraby/activities/main/settings/ProfileSettingsFragment.java
+++ app/src/main/java/com/example/vandraby/activities/main/settings/ProfileSettingsFragment.java
@@ -1,7 +1,6 @@
 package com.example.vandraby.activities.main.settings;
 
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +13,9 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.vandraby.R;
 import com.example.vandraby.activities.main.profile.ProfileFragment;
 import com.example.vandraby.information.DataModel;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileSettingsFragment extends Fragment {
 
@@ -29,7 +31,10 @@ public class ProfileSettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_profile_setting, null);
+        View view = inflater.inflate(R.layout.fragment_profile_settings, null);
+
+        CircleImageView imageUserPhoto = view.findViewById(R.id.image_user_photo);
+        Picasso.with(getContext()).load(DataModel.getInstance().getCurrentUser().getPhotoUrl()).fit().into(imageUserPhoto);
 
         CardView buttonBack = view.findViewById(R.id.button_back);
         buttonBack.setOnClickListener(v -> {
