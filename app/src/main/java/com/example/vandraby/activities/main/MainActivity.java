1df0787ff46e32ab70ad32fc2af15646ca637d28
Vanya Sachkovski <ivan.sachkovski@gmail.com>
2021-10-18 14:05:49 +0300

    change authorization activity

--- app/src/main/java/com/example/vandraby/activities/main/MainActivity.java
+++ app/src/main/java/com/example/vandraby/activities/main/MainActivity.java
@@ -4,13 +4,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.appcompat.widget.Toolbar;
 
 import com.example.vandraby.R;
 import com.example.vandraby.activities.main.profile.ProfileFragment;
